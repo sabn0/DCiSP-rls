35572e479d420aa6ff266b4f483937be9ad75298
nonowar7 <sabalanoam77@gmail.com>
2023-07-31 12:08:52 +0300

    copied for rls

---
+++ Consts/Consts.py
@@ -0,0 +1,91 @@
+
+ANSWER = 'answer'
+ANSWER_RT = 'answer_rt'
+AVGWORDREACTIONTIME = 'AvgWordReactionTime'
+
+COMPLEXINDEX = 'ComplexIndex'
+COLUMNS_NGRAMS = ['SerialIndex', 'ComplexIndex', 'Tok', 'SentenceType', 'SentenceItem', 'Lexical', 'Syntactic', 'Syntactic+Lexical']
+COLUMNS_LM = ['SerialIndex', 'ComplexIndex', 'Tok', 'SentenceType', 'SentenceItem', 'Surprisal']
+
+DATA_CELL_INDEX = 'data_cell_index'
+DATA_TYPE_CELL_INDEX = 'data_type_cell_index'
+
+END_TAG = '<EOS>'
+END_TOK = '<סוף>'
+EXPERIENCEANSWERS = 'ExperienceAnswers'
+EXPERIENCE_COLUMNS = 'Experience_columns'
+
+INDEX_MAPPING = {'SRC': { 1: 1, 2: 1, 3: 2, 4: 2, 5: 3, 6: 4, 7: 4, 8: 5}, 'ORC': { 1: 1, 2: 1, 3: 2, 4: 2, 5: 2, 6: 3, 7: 4}}
+INNER = 'inner'
+ITEM_ID = 'item_id'
+
+NUM_CELLS = 'num_cells'
+NUM_TRIALS = 'num_trials'
+
+FILL_NA = 'fill_NA'
+FILLER = 'Filler'
+FILLER_SESSION = 'filler_session'
+
+GROUP = 'Group'
+
+MIN_RT = 'min_rt'
+
+ORC = 'ORC'
+ORC_BEGINNING = 'ORC_Beginning'
+ORC_EMBEDDED = 'ORC_Embedded'
+ORC_FULL = 'ORC_Full'
+ORC_SESSION = 'orc_session'
+
+PASSAGES_IN_EXPERIENCE = 'passages_in_experience'
+PASSAGE_MIN_READING_TIME = 'passage_min_reading_time'
+PASSAGE_MULTICHOICE_ANSWERS = 'passage_multichoice_answers'
+PASSAGE_NUM_QUESTIONS = 'passage_num_questions'
+PRACTICE_STR = 'practice_str'
+PREDICTION = 'Prediction'
+
+QUESTION = 'question'
+QUESTION_STR = 'question_str'
+QUESTIONANSWER = 'QuestionAnswer'
+
+RARE_TOK = '<RARE>'
+RC = 'RC'
+REC_TITLE_CELL_INDEX = 'rec_title_cell_index'
+REC_VALUE_CELL_INDEX = 'rec_value_cell_index'
+RT = 'rt'
+RT_STR = 'rt_str'
+
+SENTENCE = 'Sentence'
+SENTENCEITEM = 'SentenceItem'
+SENTENCETYPE = 'SentenceType'
+SEQ2MIL = 1000
+SESSION = 'Session'
+SESSION2ORDER = 'session2order'
+SETTING_INDEXES_END = 'setting_indexes_end'
+SPR_COLUMNS = 'Spr_columns'
+SRC = 'SRC'
+SRC_BEGINNING = 'SRC_Beginning'
+SRC_EMBEDDED = 'SRC_Embedded'
+SRC_FULL = 'SRC_Full'
+SRC_SESSION = 'src_session'
+START_TAG = '<SOS>'
+START_TOK = '<התחלה>'
+SUB2GROUP = 'sub2group'
+SUB2SPRORDER = 'sub2sprOrder'
+SUBJECT_STR = 'subject_str'
+SUBJECTNUMBER = 'SubjectNumber'
+SURPRISAL = 'Surprisal'
+
+TOEXCLUDE = 'ToExclude'
+TOKEN_INDEX_LOC = 'token_index_loc'
+
+VALUE_CELL_INDEX = 'value_cell_index'
+
+WM_ANSWER_INDEX = 'wm_answer_index'
+WM_COLUMNS = 'WM_columns'
+WM_MAX_RESULT = 'wm_max_result'
+WM_MIN_RESULT = 'wm_min_result'
+WM_RESULTS_STR = 'wm_results_str'
+WMRESULT = 'WMResult'
+WORDREACTIONTIME = 'WordReactionTime'
+WORDINDEX = 'WordIndex'
+WORDLENGTH = 'WordLength'
